251626330a64bf0e386dc0a1161724ab7002b645
defiguem <81821644+defiguem@users.noreply.github.com>
2021-06-10 15:45:27 +0200

    Update scatter2d.m

--- plots/scatter2d.m
+++ plots/scatter2d.m
@@ -1,4 +1,4 @@
-function [res] = scatter2d(x, y, labels, ms, mkr, filled)
+function [res] = scatter2d(x, y, labels, ms, mkr, filled, convexhull)
 %
 % 2D-scatter plot of values in x and y, colored by labels.
 % 
@@ -10,6 +10,7 @@ function [res] = scatter2d(x, y, labels, ms, mkr, filled)
 % ms : marker size
 % mkr : marker type
 % filled : whether markers should be filled (1) or not (0) 
+% convexhull : whether convex hulls are drawn (1) around classes or not (0)
 %
 % Output arguments :
 % ==================
@@ -21,7 +22,7 @@ function [res] = scatter2d(x, y, labels, ms, mkr, filled)
 % ms = 30;
 % mkr = 'o';
 % filled = 1;
-% scatter2d(x, y, labels, ms, mkr, filled);
+% scatter2d(x, y, labels, ms, mkr, filled, convexhull);
 %
 % Related functions :
 % ===================
@@ -34,6 +35,8 @@ function [res] = scatter2d(x, y, labels, ms, mkr, filled)
 %
 % Modifications:
 % ==============
+% 2021.05.20 : added convex hulls drawing option in input argument;
+% 2021.06.01 : uses the matlab plot function, not scatter anymore
 %
 % =========================================================================
 
@@ -41,6 +44,10 @@ function [res] = scatter2d(x, y, labels, ms, mkr, filled)
 
 % Color assignment acording to classes
 res = classcol(labels);
+
+if nargin < 7
+    convexhull = 0;
+end
 
 if nargin < 6
     filled = 1;
@@ -56,7 +63,7 @@ end
 
 if length(mkr) == 1
     mkr = repelem(mkr,max(res.classvec));
-elseif length(mkr) ~= max(res.classvec) || isempty(mkr) == 0
+elseif length(mkr) ~= max(res.classvec) || isempty(mkr) == 1
     mkr = distmkr(max(res.classvec));
 end
 
@@ -75,11 +82,19 @@ end
 %% Main section
 
 for i = 1 : length(res.labels)
+    
+    idx = find(res.classvec == i);
+    
     if filled == 1
-        res.handles{i} = scatter(x(res.classvec == i), y(res.classvec == i), ms, res.colmat(res.classvec == i,:), 'filled', mkr{i});
+        res.handles{i} = plot(x(idx), y(idx), 'Color', res.colmat(idx(1),:), 'MarkerFaceColor', res.colmat(idx(1),:), 'Marker', mkr{i}, 'MarkerSize', ms, 'LineStyle','None');
         hold on;
     else
-        res.handles{i} = scatter(x(res.classvec == i), y(res.classvec == i), ms, res.colmat(res.classvec == i,:), mkr{i});
+        res.handles{i} = plot(x(idx), y(idx), 'Color', res.colmat(idx(1),:), 'Marker', mkr{i}, 'MarkerSize', ms, 'LineStyle','None');
+    end
+    
+    if convexhull == 1
+        k = convhull(x(idx), y(idx));
+        plot(x(idx(k)), y(idx(k)),':','Color',res.colmat(idx(1),:),'linewidth',2);
     end
 end
 
