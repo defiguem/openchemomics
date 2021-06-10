193ee587a9d92bb0baa1f36815de799a2aee9765
defiguem <81821644+defiguem@users.noreply.github.com>
2021-06-10 15:47:01 +0200

    Update scatter3d.m

--- plots/scatter3d.m
+++ plots/scatter3d.m
@@ -1,4 +1,4 @@
-function [res] = scatter3d(x, y, z, labels, ms, mkr, filled, type)
+function [res] = scatter3d(x, y, z, labels, ms, mkr, filled, convexhull)
 %
 % 3D-scatter plot of values in x, y and z, colored by labels.
 %
@@ -11,6 +11,7 @@ function [res] = scatter3d(x, y, z, labels, ms, mkr, filled, type)
 % ms : marker size
 % mkr : marker type
 % filled : whether markers should be filled (1) or not (0) 
+% convexhull : whether convex hulls are drawn (1) around classes or not (0)
 %
 % Output arguments :
 % ==================
@@ -22,7 +23,7 @@ function [res] = scatter3d(x, y, z, labels, ms, mkr, filled, type)
 % ms = 30;
 % mkr = 'o';
 % filled = 1;
-% scatter3d(x, y, labels, ms, mkr, filled);
+% scatter3d(x, y, z, labels, ms, mkr, filled, convexhull);
 %
 % Author :
 % Miguel de Figueiredo
@@ -30,13 +31,17 @@ function [res] = scatter3d(x, y, z, labels, ms, mkr, filled, type)
 %
 % Modifications:
 % ==============
+% 2021.06.01 : uses the matlab plot function, not scatter anymore
 %
 % =========================================================================
 
 %% Fail-safe section
 
+% Color assignment acording to classes
+res = classcol(labels);
+
 if nargin < 7
-    type = 'qual'; % 'qualitative' as opposed to 'sequential' ('seq')
+    convexhull = 1;
 end
 
 if nargin < 6
@@ -44,54 +49,51 @@ if nargin < 6
 end
 
 if nargin < 5
-    mkr = 'o';
+    mkr = distmkr(max(res.classvec));
+end
+
+if ischar(mkr)
+    mkr = {mkr};
+end
+
+if length(mkr) == 1
+    mkr = repelem(mkr,max(res.classvec));
+elseif length(mkr) ~= max(res.classvec) || isempty(mkr) == 0
+    mkr = distmkr(max(res.classvec));
 end
 
 if nargin < 4
-    ms = 20;
+    ms = 30;
 end
 
-if nargin < 3
-    error('At least x, y and z axes coordinates muste be provided, along with class assignment information');
+if nargin < 3 
+    error('At least x and y axes coordinates must be provided, along with class assignment information');
 end
 
-if min(size(x)) > 1 || min(size(y)) > 1 || min(size(z)) > 1
+if min(size(x)) > 1 || min(size(y)) > 1
     error('Check dimensions of x and y : only vectors are allowed');
 end
 
 %% Main section
 
-% Color assignment acording to classes
-res = classcol(labels);
-
-if strcmp(type,'qual') == 0
-    % Defines a gradient of colors between col1 and col2
-    col1 = [0, 0.4470,0.7410];
-    col2 = [0.9769 0.9839 0.0805];
-    [~,I] = sort(labels,'ascend');
-    lgth = length(labels);
-    grad = [linspace(col1(1),col2(1),lgth)',linspace(col1(2),col2(2),lgth)',linspace(col1(3),col2(3),lgth)'];
-    for i = 1 : lgth
-        res.colmat(i,:) = grad((I==i),:);
-    end
-end
-
-figure; hold on;
 for i = 1 : length(res.labels)
+    
+    idx = find(res.classvec == i);
+    
     if filled == 1
-        scatter3(x(res.classvec == i), y(res.classvec == i), ...
-            z(res.classvec == i), ms, res.colmat(res.classvec == i,:), 'filled', mkr);
+        res.handles{i} = plot3(x(idx), y(idx), z(idx), 'Color', res.colmat(idx(1),:), 'MarkerFaceColor', res.colmat(idx(1),:), 'Marker', mkr{i}, 'MarkerSize', ms, 'LineStyle','None');
+        hold on;
     else
-        scatter3(x(res.classvec == i), y(res.classvec == i), ...
-            z(res.classvec == i), ms, res.colmat(res.classvec == i,:), mkr);
+        res.handles{i} = plot3(x(idx), y(idx), z(idx), 'Color', res.colmat(idx(1),:), 'Marker', mkr{i}, 'MarkerSize', ms, 'LineStyle','None');
+    end
+    
+    if convexhull == 1
+        k = convhulln([x(idx), y(idx) z(idx)]);
+        %plot3(x(idx(k)), y(idx(k)), z(idx(k)),':','Color',res.colmat(idx(1),:),'linewidth',2);
+        trisurf(k,x(idx), y(idx), z(idx),'FaceColor',res.colmat(idx(1),:),'FaceAlpha',0.4)
     end
 end
 
 box on; axis tight;
-if strcmp(type,'qual') == 0
-    colormap(grad);
-    cbr = colorbar('northoutside');
-    set(cbr,'TickLabels',linspace(floor(min(labels)), round(max(labels)), 11));
-end
 
 end
